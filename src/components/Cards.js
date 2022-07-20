e8c68eacdd46f4b70a3ea6aeea7068b4733dbf04
DA M1 55 <dam155@DA-LEARNERs-MacBook-T2.local>
2022-07-20 14:32:00 +0200

    card-body

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,17 @@ import Card from 'react-bootstrap/Card'
 const Cards = () => {
   return (
     <>
-      
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+
+        <div className='card_body'>
+          <div className='upper_data d-flex justify-content-between align-items-center'>
+            <h4 className='mt-2'>Mduduzi</h4>
+            <span>3.8&nbsp;★</span>
+          </div>
+        </div>
+
+      </Card>
     </>
   )
 }
